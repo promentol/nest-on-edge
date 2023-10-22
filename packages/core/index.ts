7a32834ee13e806dfef0355581b3f2f40a3f51cb
narek <promentol@gmail.com>
2023-10-22 11:56:03 +0400

    Add core package

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,21 +1,36 @@
+import { NestFactory } from "@nestjs/core";
+import { HonoAdapter, NestHonoApplication } from "@noe/platform-hono";
+import { NestModule } from "@nestjs/common";
+
+
 class NestOnEdgeApp {
 
-    _app: Promise<any>;
+    _app: any;
     promise: Promise<any>;
 
     // bootsrap: any;
     AppModule: any;
 
-    constructor(AppModule) {
+    constructor(AppModule: any) {
         this.AppModule = AppModule
     }
-    async fetch(req) {
+    async fetch(...args) {
         if(!this._app) {
             this._app = await this.bootsrap(this.AppModule)
+            await this._app.init()
         }
+        return this._app.getInstance().fetch(...args)
     }
 
-    async bootsrap() {
-
+    async bootsrap(AppModule: any) {
+        const app = await NestFactory.create<NestHonoApplication>(this.AppModule, new HonoAdapter(), {
+            abortOnError: false,
+            logger: console
+        });
+        return app
     }
+}
+
+export {
+    NestOnEdgeApp
 }
